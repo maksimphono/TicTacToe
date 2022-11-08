a690a3205cf89ac9a9df601045c42f3834149c0a
Maxun <trukhinmaksimof@mail.ru>
2022-11-09 01:21:59 +0800

    Successfuly rewrited 'SognNode' as simple object

--- src/testing_win.js
+++ src/testing_win.js
@@ -1,59 +1,37 @@
 const [SIZE, FULL_ROW] = [4, 4];
 
-export class SignNode {
-    static get directionsGroups() { return [ [0, 1, '|'], [1, 0, '-'], [1, 1, 'dr'], [-1, 1, 'dl'] ]};
-    
-    constructor (mat, x, y, sign) {
-        this.groups = {'-' : null, '|' : null, 'dr' : null, 'dl' : null};
-        this.sign = sign;
-        mat[x][y] = this;
-        this.placeOnBoard(mat, x, y);
-    }
-    placeOnBoard = function (mat, x, y){
-        let pivotCell = null;
-        let oppositeCell = null;
+export default function placeOnBoard(board, x, y, sign){
+    const directionsGroups = [ [0, 1, '|'], [1, 0, '-'], [1, 1, 'dr'], [-1, 1, 'dl'] ];
+    let pivotCell = null;
+    let oppositeCell = null;
+    const newSign = {
+            groups : {'-' : null, '|' : null, 'dr' : null, 'dl' : null},
+            sign : sign
+        }
+    board[x][y] = newSign;
 
-        console.log("Place ", this.sign, x, y);
-        for (let [dx, dy, gr] of SignNode.directionsGroups){
-            this.groups[gr] = [1];
-            console.log("Check on ", dx, dy);
-            
-            if (!mat[x + dx] || !mat[x + dx][y + dy] || mat[x + dx][y + dy].sign !== this.sign)
-                [dx, dy] = [-dx, -dy]; 
-            pivotCell = mat[x + dx] && mat[x + dx][y + dy];
-            oppositeCell = mat[x - dx] && mat[x - dx][y - dy];
-            
-            if (pivotCell && pivotCell.sign === this.sign){
-                console.log("Got neighbour on ", x + dx, y + dy, gr);
-                if (oppositeCell && this.sign === oppositeCell.sign){
-                    pivotCell.groups[gr][0] += oppositeCell.groups[gr][0];
-                    oppositeCell.groups[gr] = pivotCell.groups[gr];
-                }
-                pivotCell.groups[gr][0]++;
-                this.groups[gr] = pivotCell.groups[gr];
-                console.log("This Group set up to ", this.groups[gr][0]);
-                if (this.groups[gr][0] >= FULL_ROW){
-                    return true;
-                }
+    console.log("Place ", newSign.sign, x, y);
+    for (let [dx, dy, gr] of directionsGroups){
+        newSign.groups[gr] = [1];
+        console.log("Check on ", dx, dy);
+        if (!board[x + dx] || !board[x + dx][y + dy] || board[x + dx][y + dy].sign !== newSign.sign)
+            [dx, dy] = [-dx, -dy];
+        pivotCell = board[x + dx] && board[x + dx][y + dy];
+        oppositeCell = board[x - dx] && board[x - dx][y - dy];
+    
+        if (pivotCell && pivotCell.sign === newSign.sign){
+            console.log("Got neighbour on ", x + dx, y + dy, gr);
+            if (oppositeCell && newSign.sign === oppositeCell.sign){
+                pivotCell.groups[gr][0] += oppositeCell.groups[gr][0];
+                oppositeCell.groups[gr] = pivotCell.groups[gr];
+            }
+            pivotCell.groups[gr][0]++;
+            newSign.groups[gr] = pivotCell.groups[gr];
+            console.log("newSign Group set up to ", newSign.groups[gr][0]);
+            if (newSign.groups[gr][0] >= FULL_ROW){
+                return true;
             }
         }
-        return false;
     }
+    return false;
 }
-/*
-const mat = [
-    [],
-    [],
-    [],
-    []
-]
-let [dx, dy] = [0, 0]
-let node1 = new SignNode(mat, 1, 2, "X");
-new SignNode(mat, 1, 3, "X");
-new SignNode(mat, 1, 1, "X");
-new SignNode(mat, 2, 2, "X");
-new SignNode(mat, 0, 0, "X");
-new SignNode(mat, 3, 2, "X");
-new SignNode(mat, 3, 3, "X");
-new SignNode(mat, 3, 1, "X");
-*/
--- src/views/BoardView.jsx
+++ src/views/BoardView.jsx
@@ -2,7 +2,7 @@ import {useState, useRef, useMemo} from 'react';
 import BoardCellView from "./BoardCellView.jsx";
 import {Container, Row, Col} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {SignNode} from "../testing_win.js";
+import placeOnBoard from "../testing_win.js";
 
 function range(length){
     return Array.from({ length }, (_, i) => i); 
@@ -16,7 +16,8 @@ function Board(props){
     console.log(props.playerNum);
 
     const updateMatrix = (x, y) => {
-        new SignNode(tictacmatrix, x, y, signs[0]);
+        if (placeOnBoard(tictacmatrix, x, y, signs[0]))
+            alert(signs[0] + " Won!!!");
         signs.push(signs.shift());
         //signs[0] = (signs[0] + 1) % props.playerNum;
     }
