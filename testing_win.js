bb2acfe6f84f6bb645dbca10d073768e328f8d7a
Maxun <trukhinmaksimof@mail.ru>
2022-11-08 12:20:17 +0800

    try win function with 'groups'

--- testing_win.js
+++ testing_win.js
@@ -1,70 +1,67 @@
-const mat = [
-    [..."____"], 
-    [..."____"], 
-    [..."____"],
-    [..."____"]
-]
-
 const [size, fullRow, currSign] = [4, 4, "X"];
 
 class SignNode {
-    constructor(x, y, sign){
-        [this.x, this.y, this.sign, this.neighbours, this.neighboursNum] = [x, y, sign, {}, 0];
+    static get directionsGroups() { return [ [0, 1, '|'], [1, 0, '-'], [1, 1, 'dr'], [-1, 1, 'dl'] ]};
+    
+    constructor () {
+        this.sign = '';
+        this.groups = {'-' : null, '|' : null, 'dr' : null, 'dl' : null};
     }
-    set newNeighbour(newNode) {
-        console.log("Node : ", [this.x, this.y]);
-        console.log("Set neightbour on ", [this.x - newNode.x, this.y - newNode.y]);
-        this.neighbours[[this.x - newNode.x, this.y - newNode.y]] = newNode;
-        this.neighboursNum++;
-    };
-    get friend(){
-        return this.neighbours;
-    };
-    repl = function (){
-        console.table([this.x, this.y]);
+    opposite = (mat, dx, dy) => (mat[-x] && mat[-x][-y]);
+
+    setUp = function (mat, x, y, sign){
+        this.sign = sign;
+        this.placeOnBoard(mat, x, y);
+    }
+    win = function (){
+        console.log(`${this.sign} Won!!`);
+    }
+    placeOnBoard = function (mat, x, y){
+        let pivotCell = null;
+        let oppositeCell = null;
+        let xy = [];
+        console.log("Place ", this.sign, x, y);
+        for (let [dx, dy, gr] of SignNode.directionsGroups){
+            this.groups[gr] = [1];
+            console.log("Check on ", dx, dy);
+            
+            if (!mat[x + dx] || !mat[x + dx][y + dy] || mat[x + dx][y + dy].sign !== this.sign)
+                [dx, dy] = [-dx, -dy]; 
+            pivotCell = mat[x + dx] && mat[x + dx][y + dy] || undefined;
+            oppositeCell = mat[x - dx] && mat[x - dx][y - dy] || undefined;
+            
+            if (pivotCell && pivotCell.sign === this.sign){
+                console.log("Got neighbour on ", x + dx, y + dy, gr);
+                if (oppositeCell && this.sign === oppositeCell.sign){
+                    pivotCell.groups[gr][0] += oppositeCell.groups[gr][0];
+                    oppositeCell.groups[gr] = pivotCell.groups[gr];
+                }
+                pivotCell.groups[gr][0]++;
+                this.groups[gr] = pivotCell.groups[gr];
+                console.log("This Group set up to ", this.groups[gr][0]);
+                if (this.groups[gr][0] >= fullRow){
+                    this.win();
+                }
+            }
+        }
     }
 }
 
-const chechQueue = [];
-
-const sign11 = new SignNode(1, 1, "X");
-const sign21 = new SignNode(2, 1, "X");
-const sign31 = new SignNode(3, 1, "X");
-const sign32 = new SignNode(3, 2, "X");
-
-sign11.newNeighbour = new SignNode(0, 1, 'X');
-sign11.newNeighbour = sign21;
-sign21.newNeighbour = sign31;
-sign31.newNeighbour = sign32;
-sign31.newNeighbour = new SignNode(3, 0, "X");
-
-function checkSide(node, side){
-    let length = 0;
-    if (node.friend[side] && node.friend[side].sign === node.sign){
-        length += 1 + checkSide(node.friend[side], side);
-    }
-    if (node.neighboursNum > 1){
-        checkQueue.push(node);
-    }
-    //if (length == fullRow) return "Row " + node.sign;
-    return length;
-}
-
-function checkBySides(node){
-    if (!node) return 0;
-    let length = 1;
-    for (let side in ["1,0", "-1,0", "0,1", "0,-1"]){
-        length = 1;
-        length += checkSide(node, side);
-        length += checkSide(node, side);
-        console.log(`Found row ${length}, by side ${side}`);
-        if (length == fullRow) return "Row " + node.sign;
-    }
-    return length;
-}
+const mat = [
+    [new SignNode(), new SignNode(), new SignNode(), new SignNode()], 
+    [new SignNode(), new SignNode(), new SignNode(), new SignNode()], 
+    [new SignNode(), new SignNode(), new SignNode(), new SignNode()],
+    [new SignNode(), new SignNode(), new SignNode(), new SignNode()]
+]
+let [dx, dy] = [0, 0]
+mat[1 + dx][1 + dy].setUp(mat, 1 + dx, 1 + dy, "X");
+//console.log(mat[1][1].groups);
+mat[1][2].setUp(mat, 1, 2, "X");
+mat[1][3].setUp(mat, 1, 3, "X");
+mat[2][2].setUp(mat, 2, 2, "X");
+mat[0][2].setUp(mat, 0, 2, "X");
 
 
-chechQueue = [sign11]
-for (let node of checkQueue){
-    checkHorizontal(sign);
-}
+mat[0][3].setUp(mat, 0, 3, "X");
+mat[3][0].setUp(mat, 3, 0, "X");
+mat[2][1].setUp(mat, 2, 1, "X");
