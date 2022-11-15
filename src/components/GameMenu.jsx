eabc7591e3ce797609cbb9dd576f8c4fec8327b0
Maxun <trukhinmaksimof@mail.ru>
2022-11-15 17:29:53 +0800

    continue to style main menu

--- src/components/GameMenu.jsx
+++ src/components/GameMenu.jsx
@@ -1,27 +1,30 @@
 import {useState} from "react";
 import {Button, ButtonGroup, Container} from "react-bootstrap";
 import "../css/GameMenu_style.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function GameMenu(props) {
     const [settings, changeSettings] = useState({});
 
     return (
         <Container className="d-flex justify-content-around align-items-center flex-column">
-                <h1>Wellcome to Tic Tak Toe Game</h1>
-                <div className = "d-grid gap-1 m-3">
-                    <Button variant="dark">
+                <div className = "p-3"><h1>Wellcome to Tic Tak Toe Game</h1></div>
+                <div className = "d-flex gap-1 flex-column align-items-center">
+                    <button variant="dark">
                         Start
-                    </Button>
-                    <Button variant="dark">
+                    </button>
+                    <button variant="dark">
                         Settings
-                    </Button>
+                    </button>
                     <ButtonGroup>
-                        <Button variant = "outline-dark">
-                            Log In
-                        </Button>
-                        <Button variant = "outline-dark">
-                            Sign Up                    
-                        </Button>
+                        <button className="d-flex justify-content-between align-items-center px-3 gap-1">
+                            <i class="bi bi-door-open"></i>
+                            <span>Log In</span>
+                        </button>
+                        <button className="d-flex justify-content-between align-items-center px-3 gap-1">
+                            <i className="bi bi-person-plus"></i>
+                            <span>Sign Up</span>                   
+                        </button>
                     </ButtonGroup>
                     
                 </div>
