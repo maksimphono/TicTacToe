a7473125f11ee35437a11fd7de122fa92ef6f90f
Maxun <trukhinmaksimof@mail.ru>
2022-11-23 17:47:11 +0800

    starting to make optimization using 'removeFromBoard'

--- src/pages/Board/components/minimax_algirithm.js
+++ src/pages/Board/components/minimax_algirithm.js
@@ -27,70 +27,6 @@ function isMovesLeft(board, signsNum) {
     return signsNum < (board.length ** 2);
 }
 
-function minimax(board, depth, isMax, signs, fullRow, signsNum, alpha = -1000, beta = 1000) {
-    let score = null;
-    const boardCopy = null;
-    let moves = [];
-    let move = {};
-    let best = 0;
-    let newBoard = null;
-
-    if (!isMovesLeft(board, signsNum) || depth >= 6) return 0;
-
-    if (isMax) {
-        best = -1000;
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board.length; j++){
-                
-                if (board[i][j] == null){
-                    newBoard = copyBoard(board);
-                    if (placeOnBoard(newBoard, i, j, signs[0], fullRow)){
-                        return 10 / (depth + 1);
-                    }
-                    
-                    best = Math.max(
-                        best,
-                        minimax(newBoard, depth + 1, !isMax, signs, fullRow, signsNum + 1, alpha, beta)
-                    );
-                    alpha = Math.max(alpha, best);
-                    
-                    if (beta <= alpha)
-                        break
-                }
-            }
-            if (beta <= alpha)
-                break
-        }
-        if (score) return score;
-        return best;
-    } else {
-        best = 1000;
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board.length; j++){
-                
-                if (board[i][j] == null){
-                    newBoard = copyBoard(board);
-                    if (placeOnBoard(newBoard, i, j, signs[1], fullRow)) {
-                        return -10 / (depth + 1);
-                    }
-                    
-                    best = Math.min(
-                        best,
-                        minimax(newBoard, depth + 1, !isMax, signs, fullRow, signsNum + 1, alpha, beta)
-                    );
-                    beta = Math.min(beta, best);
-
-                    if (beta <= alpha)
-                        break
-                }
-            }
-            if (beta <= alpha)
-                break
-        }
-        if (score) return score;
-        return best;
-    }
-}
 
 function getNeighbours(board, i, j) {
     const directionsGroups = [ [0, 1], [1, 0], [1, 1], [-1, 1] ];
@@ -107,6 +43,65 @@ function getNeighbours(board, i, j) {
         if (oppositeCell === null) neightbours.push([i - di, j - dj]);
     }
     return neightbours;
+}
+
+function minimax(board, depth, isMax, signs, fullRow, signsNum, alpha = -1000, beta = 1000) {
+    let score = null;
+    const boardCopy = null;
+    let moves = [];
+    let move = {};
+    let best = 0;
+    let newBoard = null;
+
+    if (!isMovesLeft(board, signsNum) || depth >= 6) return 0;
+
+    if (isMax) {
+        best = -1000;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++){
+                if (board[i][j] == null){
+                    newBoard = copyBoard(board);
+                    if (placeOnBoard(newBoard, i, j, signs[0], fullRow)){
+                        return 10 / (depth + 1);
+                    }
+                    best = Math.max(
+                        best,
+                        minimax(newBoard, depth + 1, !isMax, signs, fullRow, signsNum + 1, alpha, beta)
+                    );
+                    alpha = Math.max(alpha, best);
+                    if (beta <= alpha)
+                        break
+                }
+            }
+            if (beta <= alpha)
+                        break
+        }
+        if (score) return score;
+        return best;
+    } else {
+        best = 1000;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++){
+                if (board[i][j] == null){
+                    newBoard = copyBoard(board);
+                    if (placeOnBoard(newBoard, i, j, signs[1], fullRow)){
+                        return -10 / (depth + 1);
+                    }
+                    best = Math.min(
+                        best,
+                        minimax(newBoard, depth + 1, !isMax, signs, fullRow, signsNum + 1, alpha, beta)
+                    );
+                    beta = Math.min(beta, best);
+                    if (beta <= alpha)
+                        break
+                }
+            }
+            if (beta <= alpha)
+                break
+        }
+        if (score) return score;
+        return best;
+    }
 }
 
 export default async function findBestMove(board, placeOnBoard, winCallback, _signs, signsNum, fullRow){
@@ -149,6 +144,24 @@ export default async function findBestMove(board, placeOnBoard, winCallback, _si
     }
     /*
     
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++){
+            newBoard = copyBoard(board);
+            if (board[i][j] == null) {
+                if (placeOnBoard(newBoard, i, j, signs[0], fullRow)){
+                    bestMove = [i, j];
+                    bestVal = 10;
+                }
+
+                moveVal = minimax(newBoard, 0, false, signs, fullRow, signsNum);
+                console.log("counted value ", moveVal, "for", i, j);
+                if (moveVal > bestVal){
+                    bestMove = [i, j];
+                    bestVal = moveVal;
+                }
+            }
+        }
+    }
     */
     console.log("Best value is ", bestVal, "for", ...bestMove);
     if (bestVal == -1000){
